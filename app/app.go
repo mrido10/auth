b074c4c0d68d3b45b7d891579997d55f3d0c52ae
muhammad rido <muhammadrido19@gmail.com>
2021-10-03 00:56:15 +0700

    add handle CORS

--- app/app.go
+++ app/app.go
@@ -4,12 +4,26 @@ import (
 	"auth/config"
 	"auth/controller"
 	"log"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func StartService() {
 	route := gin.Default()
+	route.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		// AllowOriginFunc: func(origin string) bool {
+		// 	return origin == "https://github.com"
+		// },
+		MaxAge: 12 * time.Hour,
+	}))
+
 	route.POST("/register", controller.Register)
 	route.POST("/login", controller.Login)
 	route.POST("/resendActivation", controller.ReSendActivation)
